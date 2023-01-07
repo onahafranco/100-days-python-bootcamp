c021ed0b7694160ea27b830681e9b1fcbb3bc9c9
onahafranco <brendantochukwu@gmail.com>
2023-01-07 14:17:11 +0100

    Day 1 Project

---
+++ Day_1/Project/Band_Name_Generator.py
@@ -0,0 +1,9 @@
+# A code that prints band name by merging City name and Pet name
+
+print("Welcome to the Band Name Generator.\n")
+
+City = input("What's the Name of the City you grew up in\n")
+Pet = input("What's your Pet's Name\n")
+
+print("Your Band Name Could Be " + City + " " + Pet)
+
