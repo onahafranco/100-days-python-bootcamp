14c10d75a020d051e4dc7713610bc8898825c262
onahafranco <brendantochukwu@gmail.com>
2023-01-10 12:07:28 +0100

    day 2 project

---
+++ Day_2/Project/tip_calculator.py
@@ -0,0 +1,16 @@
+# A program that splits tips and bills between friends..
+
+bill = input("what is the bill($)? ")
+tip_percentage = input("what percetage tip will you like to give? ")
+
+#the value of our tip will be:
+tip = (float(bill) / int(100)) * float(tip_percentage)
+total_bill = float(bill) + float(tip)
+
+#lets know the nuber of persons splitting the bill
+spliters = input("how many people will split the bill?: ")
+
+#amount to go round rounded in two decimal places will be:
+amount = (round(total_bill / int(spliters), 2))
+
+print(f"each person will pay: ${amount}")
