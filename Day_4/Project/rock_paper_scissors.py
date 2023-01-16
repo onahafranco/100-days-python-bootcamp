e88d8afecce1542fd6ebc8d1a8aaff7e04b0a8d6
onahafranco <brendantochukwu@gmail.com>
2023-01-16 17:57:44 +0100

    project day-4

--- Day_4/Project/rock_paper_scissors.py
+++ Day_4/Project/rock_paper_scissors.py
@@ -1,6 +1,5 @@
 # check out the official game website to understand the rule of the game: https://wrpsa.com/the-official-rules-of-rock-paper-scissors
 
-
 import random
 
 rock = '''
@@ -30,21 +29,22 @@ scissors = '''
 ---.__(___)
 '''
 
-#Write your code below this line 👇
-player_choice = input("Type in 0 for rock, 1 for paper or 2 for scissors: ")
+player_choice = input(int("Type in 0 for rock, 1 for paper or 2 for scissors: "))
 comp_choice = random.randint(0, 2)
-if player_choice == "0" and comp_choice == 1:
+if player_choice == 0 and comp_choice == 1:
   print(rock + "\ncomputer chose:" + paper + "\n You Win!")
-elif player_choice == "1" and comp_choice == 0:
+elif player_choice == 1 and comp_choice == 0:
   print(paper + "\ncomputer chose:" + rock + "\n Computer Wins!")
-elif player_choice == "0" and comp_choice == 2:
+elif player_choice == 0 and comp_choice == 2:
   print(rock + "\ncomputer chose:" + scissors + "\n Computer Wins!")
-elif player_choice == "2" and comp_choice == 0:
+elif player_choice == 2 and comp_choice == 0:
   print(scissors + "\ncomputer chose:" + rock + "\n You Win!")
-elif player_choice == "1" and comp_choice == 2:
+elif player_choice == 1 and comp_choice == 2:
   print(paper + "\ncomputer chose:" + scissors + "\n Computer Wins!")
-elif player_choice == "2" and comp_choice == 1:
+elif player_choice == 2 and comp_choice == 1:
   print(scissors + "\ncomputer chose:" + paper + "\n You Win!")
+if player_choice == comp_choice:
+  print("its a draw")
 else:
-  print("A tie or An invalid choice")
+  print("A invalid choice")
 
